1a00c3ea282188d0ade9e4f86653f0af4fe65793
Misha Turnbull <mishaturnbull@gmail.com>
2023-07-21 20:22:22 -0500

    Allow multiple Kconfig's in various folders
    Keeping Kconfig close to the relevant code -> better organization
    
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- bld.py
+++ bld.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import glob
 import os
 import shlex
 import subprocess
@@ -25,6 +26,27 @@ def run_cmd(cmd, **kwargs):
             capture_output=True,
             **kwargs)
     return p.stdout.decode('utf-8')
+
+def collect_kconfigs(top, output):
+    """
+    Finds Kconfig files recursively in a given directory (:param top:).  Joins
+    their content, and produces a file (:param output:) with all their
+    contents.
+    """
+    if os.path.exists(output):
+        return [f"{output} exists; please confirm it is not needed and " + \
+                "remove it!"]
+
+    contents = ['mainmenu "TagVFS Project Configuration"\n']
+    for filename in glob.glob(top + "/**/Kconfig", recursive=True):
+        with open(filename, 'r') as fp:
+            contents = [*contents, *fp.readlines()]
+
+    # apparently, you don't "endmenu" for a mainmenu.  who knew?
+    #contents.append('endmenu')
+
+    with open(output, 'w') as fp:
+        fp.writelines(contents)
 
 ###############################################################################
 
@@ -59,19 +81,64 @@ def menuconfig(cmd):
     """
     Kconfig menuconfig TUI.
     """
-    os.system("menuconfig")
-    os.system("genconfig --header-path src/config.h")
+    ck = collect_kconfigs('src', 'Kconfig.tmp')
+    if ck:
+        return ck
+
+    mc = os.system("menuconfig Kconfig.tmp")
+    # not much point calling genconfig if menuconfig died
+    if mc == 0:
+        gc = os.system("genconfig Kconfig.tmp --header-path src/config.h")
+        if gc != 0:
+            return [f"Genconfig failed; return code {gc}.  " + \
+                    "Keeping Kconfig.tmp!"]
+    else:
+        return [f"Menuconfig failed; return code {mc}." + \
+                "  Keeping Kconfig.tmp (and not calling genconfig!)"]
+
+    os.remove('Kconfig.tmp')
+
+def defaultconfig(cmd):
+    """
+    Set all Kconfig options to default.
+    """
+    ck = collect_kconfigs('src', 'Kconfig.tmp')
+    if ck:
+        return ck
+
+    ac = os.system("alldefconfig Kconfig.tmp")
+    if ac == 0:
+        gc = os.system("genconfig Kconfig.tmp --header-path src/config.h")
+        if gc != 0:
+            return [f"Genconfig failed; return code {gc}.  " + \
+                    "Keeping Kconfig.tmp!"]
+    else:
+        return [f"Alldefconfig failed; return code {mc}." + \
+                "  Keeping Kconfig.tmp (and not calling genconfig!)"]
+
+    os.remove('Kconfig.tmp')
 
 def build(cmd):
     """
     Main CMake target.
     """
-    # check if the .config file exists
-    if not os.path.exists(".config"):
-        # it does not.  create it with all options at default
-        os.system("alldefconfig")
-    # it definitely exists now -- header-ize it
-    os.system("genconfig --header-path src/config.h")
+    out = []
+    if not os.path.exists('.config'):
+        if os.path.exists("src/config.h"):
+            # this is weird...
+            out.append(f".config is gone, but src/config.h exists.  Please" + \
+                    " fix by either deleting src/config.h, or creating " + \
+                    ".config.  `bld.py menuconfig` should help.")
+        else:
+            # nothing at all.  most likely a fresh clone.  blast out default
+            # config
+            defaultconfig('')
+    elif not os.path.exists('src/config.h'):
+        # .config existed, but the header doesn't.  need user to invoke the
+        # menuconfig command which will call genconfig and headerize it
+        out.append("config.h does not exist.  Run `bld.py menuconfig`!")
+    if len(out) > 0:
+        return out
 
     os.chdir("build")
     # call os.system instead of run_cmd -- os.system causes live,
@@ -94,14 +161,28 @@ def fullclean(cmd):
     os.chdir("build")
     os.system("rm -rf ./*")
 
+def cleanconf(cmd):
+    """
+    Cleans out generated configuration files (.config, src/config.h) and reset
+    to defaults.
+    """
+    files = [".config", ".config.old", "src/config.h"]
+    for f in files:
+        try:
+            os.remove(f)
+        except:
+            pass
+
 REGISTRY = {
         # don't remove this first 'help' entry!
         "help": bldhelp,
         "doc": doc,
         "menuconfig": menuconfig,
+        "defaultconfig": defaultconfig,
         "build": build,
         "clean": clean,
         "fullclean": fullclean,
+        "cleanconf": cleanconf,
     }
 
 ###############################################################################
--- doc/devenv.rst
+++ doc/devenv.rst
@@ -31,17 +31,31 @@ To compile:
 
    ``bld.py build``
 
+   You could also do the usual ``cd build && cmake .. && make`` -- that would
+   be okay too.
+
 To configure:
 
    ``bld.py menuconfig``
+
+.. note::
+   Though it may be tempting, don't call ``kconfiglib``'s ``menuconfig``
+   directly.  It won't work.  In order to read multiple Kconfig files from
+   various directories, ``bld.py`` collects all the Kconfig's it can find and
+   glues them together into a ``Kconfig.tmp`` which is then fed into
+   ``menuconfig``.  This file is removed automatically on a successful
+   operation, but left behind if errors are encountered.
 
 To reset the world:
 
    ``bld.py fullclean``
 
+   This is equivalent to removing everything in ``build/``.
+
 You may chain commands together in one shot to do multiple things in order:
 
    ``bld.py fullclean build``
+
 
 Running with no arguments will produce a small help output describing the
 allowed commands:
@@ -50,4 +64,12 @@ allowed commands:
 
 Compiled executables are placed in the ``build`` directory.
 
+To make Sphinx documentation, the ``doc`` target passes through its
+colon-separated command to the Sphinx makefile.  For example:
+
+   | ``bld.py doc:latexpdf``
+   | ``bld.py doc:dirhtml``
+   | ``bld.py doc:help``
+
+Sphinx generated documentation will be placed in ``doc/_build/``.
 
